83b175d3dcf877a379a05260b85ad93a5bbd276b
Nicolas Berthollet <berthollet.nicolas@gmail.com>
2025-06-19 22:34:20 +0200

    Updates

---
+++ precache-manifest.f576db18bb2f4961d10ec5dbdc75f177.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb6e09ee52bf882528502f57059e0fae",
+    "url": "/Linch022/PROJET-12-OC/index.html"
+  },
+  {
+    "revision": "e2b3e9a9061df4a7cef9",
+    "url": "/Linch022/PROJET-12-OC/static/css/2.f26574f6.chunk.css"
+  },
+  {
+    "revision": "e770f472a4f8dd003a4e",
+    "url": "/Linch022/PROJET-12-OC/static/css/main.200cd0a6.chunk.css"
+  },
+  {
+    "revision": "e2b3e9a9061df4a7cef9",
+    "url": "/Linch022/PROJET-12-OC/static/js/2.da8032c7.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Linch022/PROJET-12-OC/static/js/2.da8032c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e770f472a4f8dd003a4e",
+    "url": "/Linch022/PROJET-12-OC/static/js/main.cdd134c4.chunk.js"
+  },
+  {
+    "revision": "d6cf86eb7bff31a4f1d0",
+    "url": "/Linch022/PROJET-12-OC/static/js/runtime-main.0067afe0.js"
+  }
+]);
